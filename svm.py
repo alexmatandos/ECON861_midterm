69d084dd7df3a483916cb1b0bc09fd231bc7facf
alexmatandos <amatand@clemson.edu>
2022-04-03 16:34:46 -0400

    second commit

--- svm.py
+++ svm.py
@@ -9,8 +9,11 @@ dataset = pandas.read_csv("survey_dataset_clean.csv")
 target = dataset.iloc[:, 15].values
 data = dataset.iloc[:, 5:12].values
 
-r2_scores, accuracy_scores, confusion_matrices = kfold_template.run_kfold(data, target, 4, svm.SVC(kernel = "linear"), 1, 1)
+r2_scores, accuracy_scores, confusion_matrices, multilabel_confusion_matrices = kfold_template.run_kfold(data, target, 4, svm.SVC(kernel = "linear"), 1, 1, 1)
 
 print(r2_scores)
 print(accuracy_scores)
-print(confusion_matrices)
+for confusion_matrix in confusion_matrices:
+	print(confusion_matrix)
+for multilabel_confusion_matrix in multilabel_confusion_matrices:
+	print(multilabel_confusion_matrix)
